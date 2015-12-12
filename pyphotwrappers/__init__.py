f707585a5ad279698f93fe2b9c1bc6acaea44eee
Erik Tollerud <erik.tollerud@gmail.com>
2015-12-12 09:58:48 -0800

    update init for imports

--- pyphotwrappers/__init__.py
+++ pyphotwrappers/__init__.py
@@ -2,7 +2,15 @@
 Python wrappers for photometry tools
 """
 
-from sextractor import *
-from scamp import *
-from swarp import *
-from daophot import *
+# Affiliated packages may add whatever they like to this file, but
+# should keep this content at the top.
+# ----------------------------------------------------------------------------
+from ._astropy_init import *
+# ----------------------------------------------------------------------------
+
+# For egg_info test builds to pass, put package imports here.
+if not _ASTROPY_SETUP_:
+	from .sextractor import *
+	from .scamp import *
+	from .swarp import *
+	from .daophot import *
